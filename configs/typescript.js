888f5c71d99325b152e17763c575d24edcf152ee
Rolf Erik Lekang <me@rolflekang.com>
2018-11-24 11:18:06 +0100

    fix(typescript): Config no-undef, no-unused-vars and no-use-before-define correctly

--- configs/typescript.js
+++ configs/typescript.js
@@ -1,10 +1,12 @@
 module.exports = {
   plugins: ['typescript'],
   rules: {
-    'no-unused-vars': 0,
+    'no-undef': 0, // typescript will handle this so no need for it
+
+    'no-unused-vars': 2,
     'typescript/no-unused-vars': 2,
 
-    'no-use-before-define': 0,
+    'no-use-before-define': 2,
     'typescript/no-use-before-define': 2,
 
     'typescript/no-explicit-any': 2,
