63dbc13a6d4bea4798c5a3fdaae80d277700037c
Rolf Erik Lekang <me@rolflekang.com>
2021-12-11 10:33:44 +0100

    fix: put ts rules in overrides for ts files only

--- configs/typescript.js
+++ configs/typescript.js
@@ -1,26 +1,34 @@
 module.exports = {
   plugins: ['@typescript-eslint'],
-  rules: {
-    // typescript will handle this so no need for it
-    'no-undef': 'off',
-    'no-unused-vars': 'off',
-    'no-use-before-define': 'off',
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      rules: {
+        // typescript will handle this so no need for it
+        'no-undef': 'off',
+        'no-unused-vars': 'off',
+        'no-use-before-define': 'off',
 
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/no-use-before-define': 'error',
-    '@typescript-eslint/no-explicit-any': 'error',
-    '@typescript-eslint/explicit-member-accessibility': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/ban-ts-comment': [
-      'error',
-      {
-        'ts-expect-error': 'allow-with-description',
-        'ts-ignore': 'allow-with-description',
-        'ts-nocheck': true,
-        'ts-check': false,
-        minimumDescriptionLength: 3,
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          { argsIgnorePattern: '^_' },
+        ],
+        '@typescript-eslint/no-use-before-define': 'error',
+        '@typescript-eslint/no-explicit-any': 'error',
+        '@typescript-eslint/explicit-member-accessibility': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/ban-ts-comment': [
+          'error',
+          {
+            'ts-expect-error': 'allow-with-description',
+            'ts-ignore': 'allow-with-description',
+            'ts-nocheck': true,
+            'ts-check': false,
+            minimumDescriptionLength: 3,
+          },
+        ],
       },
-    ],
-  },
+    },
+  ],
 };
